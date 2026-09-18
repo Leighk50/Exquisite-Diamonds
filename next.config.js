bc9c368092413eec90e73f2ac4a9d91b63686990
Leighk50 <Leigh@villagelimits.co.uk>
2026-09-18 14:12:57 +0100

    Use lightweight static export for demo hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-};
-
+const nextConfig = { output: 'export', trailingSlash: true };
 module.exports = nextConfig;
